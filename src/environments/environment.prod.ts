a1556dcadc9a472c5a356805fef45e5378a973d1
Jia Li <2001989@cognizant.com>
2020-04-24 01:54:51 -0400

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+const baseUri = `http://localhost`;
+const port = `8765`
+const name = 'posts';
+
 export const environment = {
-  production: true
+  production: true,
+  postUri: `${baseUri}:${port}/post-service/${name}/`,
+  commentUri: `${baseUri}:${port}/comment-service/${name}/`
 };
